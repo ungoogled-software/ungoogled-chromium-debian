88686b9a4d918bab2eb5ab9373a979b34d3febe4
Fabien Tassin <fta@ubuntu.com>
2009-06-05 18:49:00 +0200

    * Improve the testsuite to also run tests needing a DISPLAY. This could   be achieved by using xvfb   - update debian/control   - update debian/rules   - update debian/run-test.sh

--- debian/run-test.sh
+++ debian/run-test.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+want_x=0
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -x )
+      want_x=1
+      shift ;;
+    -- ) # Stop option prcessing
+      shift
+      break ;;
+    * )
+      break ;;
+  esac
+done
+
 TEST=$1
 LOGDIR=$2
 FILTER=$3
@@ -24,24 +38,25 @@ if [ ! -d $LOGDIR ] ; then
   exit 1
 fi
 
-debug() {
-  echo "run $FILTER\necho ------------------------------------------------\\\\n\necho (gdb) bt\\\\n\nbt\n" > /tmp/gdb-cmds-$$.txt
-  echo "echo ------------------------------------------------\\\\n\necho (gdb) bt f\\\\n\nbt f\n" >> /tmp/gdb-cmds-$$.txt
-  gdb -n -batch -x /tmp/gdb-cmds-$$.txt $TEST > $LOGDIR/$TEST--gdb.txt 2>&1
-  rm -f /tmp/gdb-cmds-$$.txt
-}
+if [ $want_x -eq 1 ] ; then
+  XVFB="xvfb-run -w 5"
+  RTEST="$XVFB $TEST"
+else
+  XVFB=""
+  RTEST=$TEST
+fi
 
 if [ "Z$FILTER" != Z ] ; then
   FILTER="--gtest_filter=$FILTER"
-  echo "# Running '$TEST $FILTER' ..."
+  echo "# Running '$RTEST $FILTER' ..."
 else
-  echo "# Running '$TEST' ..."
+  echo "# Running '$RTEST' ..."
 fi
 
-$TEST $FILTER > $LOGDIR/$TEST.txt 2>&1
+$XVFB $TEST $FILTER > $LOGDIR/$TEST.txt 2>&1
 RET=$?
 if [ $RET -ne 0 ] ; then
-  echo "# '$TEST' returned with error code $RET"
+  echo "# '$RTEST' returned with error code $RET"
 fi
 
 if [ $(grep -c 'Global test environment tear-down' $LOGDIR/$TEST.txt) -eq 1 ] ; then
@@ -54,7 +69,16 @@ else
 fi
 echo
 if [ $RET -eq 139 ] ; then
-  debug
+  # debug in gdb
+  if [ $want_x -eq 1 ] ; then
+    GDB="xvfb-run -w 5 gdb"
+  else
+    GDB=gdb
+  fi
+  echo "run $FILTER\necho ------------------------------------------------\\\\n\necho (gdb) bt\\\\n\nbt\n" > /tmp/gdb-cmds-$$.txt
+  echo "echo ------------------------------------------------\\\\n\necho (gdb) bt f\\\\n\nbt f\n" >> /tmp/gdb-cmds-$$.txt
+  $GDB -n -batch -x /tmp/gdb-cmds-$$.txt $TEST > $LOGDIR/$TEST--gdb.txt 2>&1
+  rm -f /tmp/gdb-cmds-$$.txt
   echo "---- crash logs ----"
   grep -E '^Program received signal' < $LOGDIR/$TEST--gdb.txt
   sed -e '1,/^Program received signal/d' < $LOGDIR/$TEST--gdb.txt
