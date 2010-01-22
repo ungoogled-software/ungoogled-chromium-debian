5731309c9003bc87084241a64b2dc21020129601
Alexander Sack <asac@ubuntu.com>
2010-01-22 13:02:35 +0100

    * fix licensegen.sh to create fresh copyright.problems rather than appending   - update debian/licensegen.sh

--- debian/licensegen.sh
+++ debian/licensegen.sh
@@ -5,7 +5,7 @@ echo >> debian/copyright
 echo "generating main copyright dep-5 format ..."
 sh -c "cd build-tree/; ../debian/licensecheck.pl -a src/" >> debian/copyright
 echo "generating problem report (see README.copyright ..."
-sh -c "cd build-tree/; ../debian/licensecheck.pl src/" >> debian/copyright.problems
+sh -c "cd build-tree/; ../debian/licensecheck.pl src/" > debian/copyright.problems
 
 echo "appending license details ..."
 for i in debian/licenses/LICENSE.*; do
