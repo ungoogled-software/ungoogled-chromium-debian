662497b1391a4f6b81cb45faca3f982a471ebc91
James Buren <braewoods+mgh@braewoods.net>
2021-04-14 16:08:39 -0500

    Ensure curl reports errors in OBS Upload

--- .github/workflows/obs-upload.sh
+++ .github/workflows/obs-upload.sh
@@ -213,17 +213,17 @@ upload_obs()
 
     esac
 
-    curl -s -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}" -F 'cmd=deleteuploadrev' << EOF
+    curl -sS -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}" -F 'cmd=deleteuploadrev' << EOF
 user="${USERNAME}:${PASSWORD}"
 EOF
 
-    curl -s -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}" > directory.xml << EOF
+    curl -sS -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}" > directory.xml << EOF
 user="${USERNAME}:${PASSWORD}"
 EOF
 
     xmlstarlet sel -t -v '//entry/@name' < directory.xml | while read FILENAME
     do
-        curl -s -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}/${FILENAME}?rev=upload" -X DELETE << EOF
+        curl -sS -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}/${FILENAME}?rev=upload" -X DELETE << EOF
 user="${USERNAME}:${PASSWORD}"
 EOF
     done
@@ -233,12 +233,12 @@ EOF
     for FILE in "${ROOT}"/*
     do
         FILENAME="${FILE##*/}"
-        curl -s -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}/${FILENAME}?rev=upload" -T "${FILE}" << EOF
+        curl -sS -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}/${FILENAME}?rev=upload" -T "${FILE}" << EOF
 user="${USERNAME}:${PASSWORD}"
 EOF
     done
 
-    curl -s -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}" -F 'cmd=commit' << EOF
+    curl -sS -K - "https://api.opensuse.org/source/${REPOSITORY}/${PACKAGE}" -F 'cmd=commit' << EOF
 user="${USERNAME}:${PASSWORD}"
 EOF
 }
