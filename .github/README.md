2f497418af5c01ee83700623a4148f49092b39c3
James Buren <braewoods+mgh@braewoods.net>
2021-10-15 15:53:34 -0500

    Initial commit of new unified Debian packaging

---
+++ .github/README.md
@@ -0,0 +1,82 @@
+# ungoogled-chromium-debian
+
+This repository contains files to build Debian packages of
+[ungoogled-chromium](//github.com/Eloston/ungoogled-chromium).
+
+These are the new unified packaging files which are designed to be built
+directly from the git repository and serve as a single set of packaging
+files for all Debian or Ubuntu releases newer than the currently oldest
+supported release, `Focal`.
+
+Even so we will only be supporting a subset of the available distributions.
+These are currently:
+- Debian Bullseye
+- Debian Sid
+- Ubuntu Focal
+- Ubuntu Impish
+
+The only guarantee we will make for support longevity is as follows:
+- Debian stable releases will be supported at least until the next stable release is available.
+- Ubuntu LTS releases will be supported at least until the next LTS release is available.
+- Ubuntu regular releases will be supported until their normal EOL with Ubuntu upstream.
+
+The actual time we decide to drop support for a release after these windows
+have elapsed will depend on what we have to gain from doing so. Examples of
+reasons we may drop a release include: upgrading to a newer toolchain and
+reintroduction of system libraries.
+
+## Getting OBS packages
+
+Use the following instructions to setup your system for our OBS repositories. Make sure to use the one for the correct distribution release for your installation.
+- Debian Bullseye
+  ```sh
+  # echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Bullseye/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+  # curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Bullseye/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+  # sudo apt update
+  # sudo apt install -y ungoogled-chromium
+  ```
+- Debian Sid
+  ```sh
+  # echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Sid/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+  # curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Sid/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+  # sudo apt update
+  # sudo apt install -y ungoogled-chromium
+  ```
+- Ubuntu Focal
+  ```sh
+  # echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+  # curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+  # sudo apt update
+  # sudo apt install -y ungoogled-chromium
+  ```
+- Ubuntu Impish
+  ```sh
+  # echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Impish/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+  # curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Impish/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+  # sudo apt update
+  # sudo apt install -y ungoogled-chromium
+  ```
+
+## Building a binary package
+
+```sh
+# Install initial packages
+sudo apt install -y devscripts equivs
+
+# Clone repository and switch to it (optional if are already in it)
+git clone https://github.com/ungoogled-software/ungoogled-chromium-debian.git
+cd ungoogled-chromium-debian
+
+# Initiate the submodules (optional if they are already initiated)
+git submodule update --init --recursive
+
+# Prepare the local source
+debian/rules setup
+
+# Install missing packages
+sudo mk-build-deps -i debian/control
+rm ungoogled-chromium-build-deps_*
+
+# Build the package
+dpkg-buildpackage -b -uc
+```
---
+++ .github/workflows/obs-upload.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+set -e
+
+RT_DIR="$PWD"
+DB_DIR="$RT_DIR/debian"
+UC_DIR="$DB_DIR/submodules/ungoogled-chromium"
+
+for i in OBS_API_USERNAME OBS_API_PASSWORD
+do
+    if test -z "$(eval echo \$$i)"
+    then
+        echo "$i is not in the environment. Aborting."
+        exit 1
+    fi
+done
+
+PROJECT="${OBS_API_PROJECT:-home:$OBS_API_USERNAME}"
+
+case "$GITHUB_EVENT_NAME" in
+    workflow_dispatch) REPOSITORY="$PROJECT:testing" ;;
+    push) REPOSITORY="$PROJECT" ;;
+    *) echo "Not running as part of a GitHub workflow. Aborting."; exit 1 ;;
+esac
+
+curl()
+{
+    for i in `seq 1 5`
+    do
+        {
+            command curl -sS -K - "$@" << EOF
+user="$OBS_API_USERNAME:$OBS_API_PASSWORD"
+EOF
+        } && return 0 || sleep 30s
+    done
+    return 1
+}
+
+debian/rules changelog control
+
+read UC_VERSION < $UC_DIR/chromium_version.txt
+AUTHOR=`grep 'AUTHOR *:=' $DB_DIR/rules | cut -f 2 -d '=' | sed -r 's;^ *| *$;;g'`
+NODE_VERSION=`grep 'NODE_VERSION *:=' $DB_DIR/rules | cut -f 2 -d '=' | sed -r 's;^ *| *$;;g'`
+
+cat > _service << EOF
+<services>
+    <service name="download_url">
+        <param name="url">https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$UC_VERSION.tar.xz</param>
+    </service>
+    <service name="download_url">
+        <param name="url">https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$UC_VERSION.tar.xz.hashes</param>
+    </service>
+    <service name="download_url">
+        <param name="url">https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz</param>
+    </service>
+    <service name="download_url">
+        <param name="url">https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-arm64.tar.xz</param>
+    </service>
+    <service name="download_url">
+        <param name="url">https://nodejs.org/dist/$NODE_VERSION/SHASUMS256.txt</param>
+        <param name="filename">node-$NODE_VERSION.sha256sum</param>
+    </service>
+</services>
+EOF
+
+tar -c -T/dev/null | xz -9 > ungoogled-chromium_$UC_VERSION.orig.tar.xz
+tar -c -T/dev/null | xz -9 > ungoogled-chromium_$UC_VERSION.debian.tar.xz
+tar -c --exclude-vcs --exclude='download_cache/*' -C $RT_DIR debian/ | xz -9 > ungoogled-chromium.tar.xz
+
+cat > ungoogled-chromium.dsc << EOF
+Format: 3.0 (quilt)
+Source: ungoogled-chromium
+Binary: ungoogled-chromium
+Architecture: any
+Version: $UC_VERSION
+Maintainer: $AUTHOR
+Homepage: https://github.com/Eloston/ungoogled-chromium
+Standards-Version: 4.5.0
+Build-Depends: $(sed -n '/^Build-Depends:/,/^$/p' < $DB_DIR/control | tr -d '\n' | cut -f 2 -d : | sed -r 's;^ *| *$;;g')
+Package-List:
+ ungoogled-chromium deb web optional arch=any
+Checksums-Sha1:
+ $(sha1sum < ungoogled-chromium_$UC_VERSION.orig.tar.xz | cut -f 1 -d ' ') $(stat -c '%s' ungoogled-chromium_$UC_VERSION.orig.tar.xz) ungoogled-chromium_$UC_VERSION.orig.tar.xz
+ $(sha1sum < ungoogled-chromium_$UC_VERSION.debian.tar.xz | cut -f 1 -d ' ') $(stat -c '%s' ungoogled-chromium_$UC_VERSION.debian.tar.xz) ungoogled-chromium_$UC_VERSION.debian.tar.xz
+Checksums-Sha256:
+ $(sha256sum < ungoogled-chromium_$UC_VERSION.orig.tar.xz | cut -f 1 -d ' ') $(stat -c '%s' ungoogled-chromium_$UC_VERSION.orig.tar.xz) ungoogled-chromium_$UC_VERSION.orig.tar.xz
+ $(sha256sum < ungoogled-chromium_$UC_VERSION.debian.tar.xz | cut -f 1 -d ' ') $(stat -c '%s' ungoogled-chromium_$UC_VERSION.debian.tar.xz) ungoogled-chromium_$UC_VERSION.debian.tar.xz
+Files:
+ $(md5sum < ungoogled-chromium_$UC_VERSION.orig.tar.xz | cut -f 1 -d ' ') $(stat -c '%s' ungoogled-chromium_$UC_VERSION.orig.tar.xz) ungoogled-chromium_$UC_VERSION.orig.tar.xz
+ $(md5sum < ungoogled-chromium_$UC_VERSION.debian.tar.xz | cut -f 1 -d ' ') $(stat -c '%s' ungoogled-chromium_$UC_VERSION.debian.tar.xz) ungoogled-chromium_$UC_VERSION.debian.tar.xz
+EOF
+
+cat > build.script << EOF
+export LANG=C.UTF-8
+
+tar -x -f ../SOURCES/ungoogled-chromium.tar.xz
+mkdir -p debian/download_cache
+ln -s ../../../SOURCES/chromium-$UC_VERSION.tar.xz debian/download_cache/chromium-$UC_VERSION.tar.xz
+ln -s ../../../SOURCES/chromium-$UC_VERSION.tar.xz.hashes debian/download_cache/chromium-$UC_VERSION.tar.xz.hashes
+ln -s ../../../SOURCES/node-$NODE_VERSION-linux-x64.tar.xz debian/download_cache/node-$NODE_VERSION-linux-x64.tar.xz
+ln -s ../../../SOURCES/node-$NODE_VERSION-linux-arm64.tar.xz debian/download_cache/node-$NODE_VERSION-linux-arm64.tar.xz
+ln -s ../../../SOURCES/node-$NODE_VERSION.sha256sum debian/download_cache/node-$NODE_VERSION.sha256sum
+
+debian/rules setup
+
+dpkg-buildpackage -b -uc
+EOF
+
+PACKAGE="ungoogled-chromium-debian"
+
+curl "https://api.opensuse.org/source/$REPOSITORY/$PACKAGE" -F 'cmd=deleteuploadrev'
+
+curl "https://api.opensuse.org/source/$REPOSITORY/$PACKAGE" > directory.xml
+
+xmlstarlet sel -t -v '//entry/@name' < directory.xml | while read FILENAME
+do
+    curl "https://api.opensuse.org/source/$REPOSITORY/$PACKAGE/$FILENAME?rev=upload" -X DELETE
+done
+
+for FILENAME in _service ungoogled-chromium_$UC_VERSION.orig.tar.xz ungoogled-chromium_$UC_VERSION.debian.tar.xz ungoogled-chromium.tar.xz ungoogled-chromium.dsc build.script
+do
+    curl "https://api.opensuse.org/source/$REPOSITORY/$PACKAGE/$FILENAME?rev=upload" -T "$FILENAME"
+done
+
+curl "https://api.opensuse.org/source/$REPOSITORY/$PACKAGE" -F 'cmd=commit'
+
+rm -f _service ungoogled-chromium_$UC_VERSION.orig.tar.xz ungoogled-chromium_$UC_VERSION.debian.tar.xz ungoogled-chromium.tar.xz ungoogled-chromium.dsc build.script directory.xml
